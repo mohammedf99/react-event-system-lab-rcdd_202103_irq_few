ffa01123754524ed870b11ceda377472e56317fe
Mohammed Mahmood <mohammed.hamidf99@gmail.com>
2021-06-10 14:54:34 +0000

    Automatically backed up by Learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -5,7 +5,9 @@ const EyesOnMe = () => {
   return(
     
     <div>
-      <button onClick={() => console.log('Good!')} onBlur={() => {console.log('Hey! Eyes on me!')}}>Click</button>
+      <button 
+      onClick={() => console.log('Good!')} 
+      onBlur={() => console.log("")}>Click</button>
     </div>
     
   )
